9e88bd7ecd4c40e04c23551b8a9d81cce5bf981e
bonnou <earthly.desire@gmail.com>
2020-11-15 15:51:00 +0900

    use Stack, Tab, Alert

--- App.tsx
+++ App.tsx
@@ -1,28 +1,221 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, Text, View, FlatList, Button, TextInput, Alert } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Screen, screensEnabled } from 'react-native-screens';
 
-export default function App() {
+type RootStackParamList = {
+  Home: {
+    post: string
+  };
+  Details: {
+    itemId: number,
+    msg: string | null,
+  };
+  CreatePost: undefined
+};
+const RootStack = createStackNavigator<RootStackParamList>();
 
+type HomeScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'Home'
+>;
+type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+type HomeProps = {
+  route: HomeScreenRouteProp,
+  navigation: HomeScreenNavigationProp
+};
+function HomeScreen({route, navigation}: HomeProps) {
   const [label, setLabel] = useState("");
+  useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+  const confirmNavigateCreatePost = () => {
+    if (Platform.OS === "web") {
+      if (confirm("My Alert Msg")) {
+        navigation.navigate('CreatePost');
+      }
+    } else {
+      Alert.alert(
+        "Alert Title",
+        "My Alert Msg",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          {
+            text: "OK",
+            onPress: () => navigation.navigate('CreatePost')
+          }
+        ],
+        { cancelable: false }
+      );
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! {label}</Text>
-      <View style={styles.buttonContainer}>
-        <Button
-          onPress={() => setLabel(label + "文字追加！")}
-          title="ボタン！"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-          onPress={() => setLabel("")}
-          title="初期化！"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-      <StatusBar style="auto" />
+    <Text>Open up App.tsx to start working on your app! {label}</Text>
+    <View style={styles.buttonContainer}>
+      <Button
+        onPress={() => setLabel(label + "文字追加！")}
+        title="ボタン！"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Button
+        onPress={() => setLabel("")}
+        title="初期化！"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Button
+        title="Create post"
+        onPress={() => confirmNavigateCreatePost()}
+      />
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          navigation.navigate('Details', {
+            itemId: 86,
+            msg: "anything you want here"
+          })
+        }}
+      />
+
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
+    <StatusBar style="auto" />
+  </View>
+  );
+}
+
+type DetailsScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'Details'
+>;
+type DetailsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Details'
+>;
+type DetailsProps = {
+  route: DetailsScreenRouteProp,
+  navigation: DetailsScreenNavigationProp,
+
+};
+function DetailsScreen({route, navigation}: DetailsProps) {
+  const param = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId:[{param.itemId}]</Text>
+      <Text>msg:[{param.msg}]</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details', {
+          itemId: 87,
+          msg: null,
+        })}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home', {post: "re-initialPost"})} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+type CreatePostScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'CreatePost'
+>;
+type CreatePostProps = {
+  navigation: CreatePostScreenNavigationProp,
+
+};
+function CreatePostScreen({navigation}: CreatePostProps) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
+  );
+}
+
+function MainScreen() {
+  return (
+    <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Overview' }}
+        initialParams={{post: "initialPost"}}
+      />
+      <RootStack.Screen
+        name="Details"
+        component={DetailsScreen}
+      />
+      <RootStack.Screen
+        name="CreatePost"
+        component={CreatePostScreen}
+      />
+    </RootStack.Navigator>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function SampleScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sample!</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Main" component={MainScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Sample" component={SampleScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- README.md
+++ README.md
@@ -7,20 +7,21 @@ list knowledge management system(React Native, TypeScript)
 * markdown preview：ctrl + k ＞ v
 
 ## usage (local debug)
-* 【install npm, yarn】
+* 【install npm, yarn, React Developer Tools】
 1. npm install --global expo-cli
 2. npm install --global yarn
 3. git clone https://github.com/sohrin/liiist.git
 4. cd liiist
 5. yarn
 * yarn run web
-* yarn run android
-* yarn run ios
+* 【read expo qr at ios or android】 
 
 ## usage (yarn history)
 * yarn add @react-navigation/native
 * expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
-* 
+* yarn add @react-navigation/stack
+* yarn add @react-navigation/bottom-tabs
+* yarn add react-native-simple-dialogs
 
 ## build
 
@@ -33,12 +34,38 @@ list knowledge management system(React Native, TypeScript)
 <dd>https://qiita.com/rubytomato@github/items/1696530bb9fd59aa28d8</dd>
 <dt>React Native の習得：コンポーネント[button]</dt>
 <dd>https://qiita.com/miyamocchi/items/544442483c01db78844b</dd>
-<dt>React-Navigatorを利用してみる（基礎編）/dt>
-<dd>https://qiita.com/zaburo/items/5b69a65db1b4d181896a</dd>
+<dt>今更だけど「Function Component」 と 「Class Component」の違いをざっくり。</dt>
+<dd>https://qiita.com/Kouichi_Itagaki/items/c8e05f084fe88a086100</dd>
+<dt>Getting started (React Navigation)</dt>
+<dd>https://reactnavigation.org/docs/getting-started</dd>
+<dt>【v4 -> v5変更点、v5使い方】react-navigationV5でちゃった・・・// 【react-native】</dt>
+<dd>https://qiita.com/YutamaKotaro/items/e3bc6a16237fc241ef7c</dd>
+<dt>[React Native] 基本を学ぶ – Propsでコンポーネントのパラメータを設定する</dt>
+<dd>https://dev.classmethod.jp/articles/react-native-props/</dd>
+<dt>propsとstateのイメージをつかむ【はじめてのReact】</dt>
+<dd>https://qiita.com/rio_threehouse/items/7632f5a593cf218b9504</dd>
+<dt>Alert: implementation</dt>
+<dd>https://github.com/necolas/react-native-web/issues/1026</dd>
+<dt>Attempted import error: 'shouldUseActivityState' is not exported from 'react-native-screens'. </dt>
+<dd>https://github.com/react-navigation/react-navigation/issues/8993</dd>
+<dt>Platform Specific Code</dt>
+<dd>https://reactnative.dev/docs/platform-specific-code</dd>
 <dt></dt>
 <dd></dd>
 <dt></dt>
 <dd></dd>
+<dt></dt>
+<dd></dd>
+<dt></dt>
+<dd></dd>
+<dt></dt>
+<dd></dd>
+</dl>
+
+## ref(not finished)
+<dl>
+<dt>Android Studioを立ち上げるのが億劫になってコマンドラインだけで済ませたAndroid出身React Nativeエンジニアの指の動き</dt>
+<dd>https://blog.nkzn.info/entry/2018/05/31/114939</dd>
 <dt></dt>
 <dd></dd>
 <dt></dt>
